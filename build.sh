838769d53312fddc0ea6fc7b735f83d5a8e48260
Martin <martin.collberg@gmail.com>
2014-11-27 17:40:39 +0100

    added comments in build.sh, gonna make a Makefile

--- build.sh
+++ build.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-. clean.sh
+
+# TODO: make a Makefile for this stuff.
+. clean.sh # Clean up old output files.
+
 arm-none-eabi-as -g -mthumb -mcpu=cortex-m4 -o startup.o startup.s
 arm-none-eabi-gcc -mthumb -mcpu=cortex-m4 -c -std=gnu99 -Os -flto -fuse-linker-plugin -o main.o main.c
 arm-none-eabi-ld -T linker.ld -o test.elf startup.o main.o
 arm-none-eabi-objcopy -O ihex -R .eeprom test.elf test.hex
-#arm-none-eabi-objcopy -O binary -R .eeprom crt0.elf crt0.bin
+#arm-none-eabi-objcopy -O binary -R .eeprom test.elf test.bin
